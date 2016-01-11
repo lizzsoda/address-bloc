107262bd98379188c013cdadf492b16f3b43f006
LizzSoda <Lizzsoda@aol.com>
2016-01-11 12:48:37 -0600

    finished the assignment

--- spec/entry_spec.rb
+++ spec/entry_spec.rb
@@ -1,22 +1,28 @@
 require_relative '../models/entry'
 
-# #1
-Rspec.describe Entry do
-  # #2
-  describe "attributes" do
-    # #3
-    it "should respond to name" do
-      entry = Entry.new
-      # #4
-      expect(entry).to respond_to (:name)
-    end
-    it "should respond to phone number" do
-      entry = Entry.newexpect(entry).to respond_to (:phone_number)
-    end
+RSpec.describe Entry do
+   describe "attributes" do
+     it "should respond to name" do
+       entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+       expect(entry).to respond_to(:name)
+     end
 
-    it "should respond to email" do
-      entry = Entry.new
-      expect(entry).to respond_to(:email)
-    end
-  end
-end
+     it "should respond to phone_number" do
+       entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+       expect(entry).to respond_to(:phone_number)
+     end
+
+     it "should respond to email" do
+       entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+       expect(entry).to respond_to(:email)
+     end
+   end
+
+   describe "#to_s" do
+     it "prints an entry as a string" do
+       entry = Entry.new('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+       expected_string = "Name: Ada Lovelace\nPhone Number: 010.012.1815\nEmail: augusta.king@lovelace.com"
+       expect(entry.to_s).to eq(expected_string)
+     end
+   end
+ end
