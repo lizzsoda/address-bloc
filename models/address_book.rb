713f02e78350907b51d7eaf98d9040edbd6e83f2
LizzSoda <Lizzsoda@aol.com>
2016-01-19 10:56:58 -0600

    Assignment 22 completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -8,18 +8,6 @@ class AddressBook
     @entries = []
   end
 
-  def remove_entry(name, phone_number, email)
-    delete_entry = nil
-
-    @entries.each do |entry|
-      if name == entry.name && phone_number == entry.phone_number && email == entry.email
-        delete_entry = entry
-      end
-    end
-
-    @entries.delete(delete_entry)
-  end
-
   def add_entry(name,phone_number,email)
     index = 0
     @entries.each do |entry|
@@ -31,14 +19,6 @@ class AddressBook
     @entries.insert(index, Entry.new(name, phone_number, email))
   end
 
-<<<<<<< HEAD
-# Search AddressBook for a specific entry by name
-
- def binary_search(name)
-   return nil
- end
-
-=======
   def import_from_csv(file_name)
     csv_text = File.read(file_name)
     csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
@@ -48,5 +28,41 @@ class AddressBook
       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
     end
   end
->>>>>>> assignment_21
+
+  # new imput for assignment 22 -->
+
+  def iterative_search(name)
+    @entries.each do |entry|
+      if entry.name == name
+        return entry
+      end
+    end
+    return nil 
+  end
+
+  def binary_search(name)
+    # #1
+    lower = 0
+    upper = @entries.length - 1
+
+# #2
+    while lower <= upper
+# #3
+      mid = (lower + upper) / 2
+      mid_name = @entries[mid].name
+
+# #4
+      if name == mid_name
+        return @entries[mid]
+      elsif name < mid_name
+        upper = mid - 1
+      elsif name > mid_name
+        lower = mid + 1
+      end
+    end
+
+# #5
+    return nil
+  end
+
 end
